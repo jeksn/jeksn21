da9c7b744558720991fcdb20925f4510b79a8750
Johan <johan@wtmg.se>
2022-03-03 08:31:05 +0100

    installed tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,11 +11,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>jeksn / Johan Eriksson</h1>
-
-        <span className={styles.info}>developer | WTMG Create</span>
-
-        <p className={styles.email}>johan@jeksn.me</p>
+        <h1 className="text-5xl font-bold">jeksn / Johan Eriksson</h1>
+        <span className="mt-4 text-xl">developer | WTMG Create</span>
+        <Link href="mailto:johan@jeksn.me">
+          <a className="mt-4 text-2xl hover:underline">johan@jeksn.me</a>
+        </Link>
       </main>
     </div>
   );
