6f3984d278e15edfe08444a87f40730420bac980
Johan <johan@wtmg.se>
2022-03-03 15:52:39 +0100

    content update

--- content/new-site.md
+++ content/new-site.md
@@ -3,10 +3,16 @@ title: "New (but actually updated) site"
 publishedDate: "2022-03-03"
 ---
 
+I finally got around to updating this old thing, or at least get started on it. The reason being that I want to start writing a bit more.
+But the problem today is that so many services lock you in and I want to be in control of my own stuff and do it in the way the internet used to work.
+So a regular ol' blog build around files that I can move anywhere.
+
+Still the tech behind the scenes isn't as old school and I still wanted to learn something while tinkering with this.
+
 ## So what is behind the scenes?
 
 Every developer is a bit obsessed with the tech powering sites and I'm really no different. Settling for something is insanely difficult when there are several options for every problem you want to solve.
-So the basics here are:
+So the basics here are...
 
 ### NextJS for the frontend
 
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Head from "next/head";
+
 import matter from 'gray-matter'
 import ReactMarkdown from 'react-markdown'
 import Link from 'next/link'
@@ -8,11 +10,16 @@ function PostTemplate({ content, data }) {
  
    return (
      <>
+      <Head>
+         <title>{frontmatter.title} - jeksn.me</title>
+         <link rel="icon" href="/favicon.ico" />
+       </Head>
      <Link href="/">
       <a className="absolute top-10 left-10 text-neutral-900">go back</a>
      </Link>
-     <div className="container flex flex-col justify-center h-full max-w-screen-md mx-auto mt-32 prose">
-       <h1>{frontmatter.title}</h1>
+     <div className="container flex flex-col justify-center h-full max-w-screen-md mx-auto my-32 prose">
+       <h1 className="mb-0">{frontmatter.title}</h1>
+       <h3 className="mt-2 mb-16 text-base font-normal text-neutral-600">{frontmatter.publishedDate}</h3>
        <ReactMarkdown children={content} />
      </div>
      </>
