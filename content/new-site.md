263fbe9ffe910d251096bc4011b43d1bcdfb80bf
Johan <johan@wtmg.se>
2022-03-03 13:17:22 +0100

    blog test

---
+++ content/new-site.md
@@ -0,0 +1,21 @@
+---
+title: "New (but actually updated) site"
+date: "2022-03-03"
+---
+
+## So what is behind the scenes?
+
+Every developer is a bit obsessed with the tech powering sites and I'm really no different. Settling for something is insanely difficult when there are several options for every problem you want to solve.
+So the basics here are:
+
+### NextJS for the frontend
+
+I just needed something quick when I first set this site up and next was the hottest thing (and still is for the most part) so why not just work with what I already have. Considered switching to Remix just to try it out, but that might be something that I might do if it happens to surpass Next in popularity.
+
+### Vercel for hosting
+
+Just to keep it simple. The other option I wanted to try for this was Cloudflare Pages, but again maybe later.
+
+### Content management
+
+This was the hardest part to settle on since there are WAY too many to pick from.
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,60 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
 
-export default function Home() {
+import fs from 'fs';
+import matter from 'gray-matter';
+import Link from 'next/link';
+
+export async function getStaticProps() {
+  const files = fs.readdirSync('content');
+
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+    const readFile = fs.readFileSync(`content/${fileName}`, 'utf-8');
+    const { data: frontmatter } = matter(readFile);
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default function Home({ posts }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Johan Eriksson - jeksn.me</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-neutral-800">
+       <Head>
+         <title>Johan Eriksson - jeksn.me</title>
+         <link rel="icon" href="/favicon.ico" />
+       </Head>
 
-      <main className={styles.main}>
-        <h1 className="text-6xl font-bold">jeksn / Johan Eriksson</h1>
-        <span className="mt-4 text-lg">developer | WTMG Create</span>
-        <Link href="mailto:johan@jeksn.me">
-          <a className="mt-4 text-2xl hover:underline">johan@jeksn.me</a>
-        </Link>
-      </main>
+       <main className="flex flex-col items-center justify-center py-6">
+         <h1 className="text-6xl font-bold">jeksn / Johan Eriksson</h1>
+         <span className="mt-4 text-lg">developer | WTMG Create</span>
+         <Link href="mailto:johan@jeksn.me">
+           <a className="mt-4 text-2xl hover:underline">johan@jeksn.me</a>
+         </Link>
+          <div className="mt-8">
+            <h2 className="text-lg font-semibold text-center">Blog posts</h2>
+            {posts.map(({ slug, frontmatter }) => (
+              <div
+                key={slug}
+                className='text-black'
+              >
+                <Link href={`/blog/${slug}`}>
+                  <a>
+                    <p className='pt-4 mb-0 text-lg underline'>{frontmatter.title}</p>
+                  </a>
+                </Link>
+                <span className='text-base'>{frontmatter.date}</span>
+              </div>
+            ))}
+          </div>
+        </main>
     </div>
   );
 }
