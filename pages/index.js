6c8e9e3fe75cf0bcbaf0f9a56c9131c077731070
Johan <johan@wtmg.se>
2022-03-14 09:16:05 +0100

    small design changes

--- pages/index.js
+++ pages/index.js
@@ -26,38 +26,32 @@ export async function getStaticProps() {
 
 export default function Home({ posts }) {
   return (
-    <div className="flex flex-col items-center justify-center h-auto bg-white md:h-screen text-neutral-800">
-       <Head>
-         <title>Johan Eriksson - jeksn.me</title>
-         <link rel="icon" href="/favicon.ico" />
-          <script async defer data-website-id="4523f431-eed9-4deb-a867-24ef52eb5d5d" src="https://umami-mu-nine.vercel.app/umami.js"></script>
-       </Head>
-
+    <div className="flex flex-col items-center justify-center h-auto bg-neutral-800 md:h-screen text-neutral-800">
        <main className="relative flex flex-col justify-center w-full md:flex-row">
          <div className="relative top-0 flex flex-col w-full min-h-[80vh] text-gray-200 md:min-h-screen bg-neutral-800 gradient md:w-1/2 ">
           <div className="container bottom-0 flex flex-col justify-end h-full px-4 pt-16 pb-4 md:px-16 md:fixed">
-            <h1 className="text-3xl font-bold">jeksn</h1>
+            <h1 className="text-4xl font-bold lg:text-7xl xl:text-9xl md:text-6xl">jeksn <br></br>/ Johan <br></br>Eriksson</h1>
             <div className="pl-3 my-6 border-l-2 border-white">
-              <p className="text-sm leading-relaxed text-[#e5e5e5]">
+              {/* <p className="text-sm leading-relaxed text-[#e5e5e5]">
                 Welcome to my corner of the internet. <br></br>I work as a developer at WTMG Create in Stockholm, Sweden. <br></br>
                 Here I'll put stuff that I find interesting and hopefully you will too.
-              </p>
+              </p> */}
             </div>
-            <p className="mb-2 text-lg ">You can find me here:</p>
-            <a href="https://twitter.com/jeksn_" className="text-base hover:underline" target="_blank">Twitter</a>
-            <a href="https://twitter.com/jeksn_" className="mb-2 text-sm italic" target="_blank">@jeksn_</a>
-            <a href="https://www.linkedin.com/in/jeksn/" className="text-base hover:underline" target="_blank">LinkedIn</a>
-            <a href="https://www.linkedin.com/in/jeksn/" className="mb-2 text-sm italic" target="_blank">@jeksn</a>
-            <a href="https://letterboxd.com/jeksn/" className="text-base hover:underline" target="_blank">Letterboxd</a>
-            <a href="https://letterboxd.com/jeksn/" className="mb-2 text-sm italic" target="_blank">@jeksn</a>
-            <a href="https://oku.club/user/jek5n" className="text-base hover:underline" target="_blank">Oku</a>
-            <a href="https://oku.club/user/jek5n" className="mb-2 text-sm italic" target="_blank">@jek5n</a>
-            <a href="mailto:johan@jeksn.me" className="text-base hover:underline">E-mail me</a>
-            <a href="mailto:johan@jeksn.me" className="mb-2 text-sm italic">johan@jeksn.me</a>
+            <p className="mb-2 text-lg">You can find me here:</p>
+            <a href="https://twitter.com/jeksn_" className="text-base w-fit hover:underline" target="_blank">Twitter</a>
+            <a href="https://twitter.com/jeksn_" className="mb-2 text-sm italic w-fit" target="_blank">@jeksn_</a>
+            <a href="https://www.linkedin.com/in/jeksn/" className="text-base w-fit hover:underline" target="_blank">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/jeksn/" className="mb-2 text-sm italic w-fit" target="_blank">@jeksn</a>
+            <a href="https://letterboxd.com/jeksn/" className="text-base w-fit hover:underline" target="_blank">Letterboxd</a>
+            <a href="https://letterboxd.com/jeksn/" className="mb-2 text-sm italic w-fit" target="_blank">@jeksn</a>
+            <a href="https://oku.club/user/jek5n" className="text-base w-fit hover:underline" target="_blank">Oku</a>
+            <a href="https://oku.club/user/jek5n" className="mb-2 text-sm italic w-fit" target="_blank">@jek5n</a>
+            <a href="mailto:johan@jeksn.me" className="text-base w-fit hover:underline">E-mail me</a>
+            <a href="mailto:johan@jeksn.me" className="mb-2 text-sm italic w-fit">johan@jeksn.me</a>
             <span className="pt-8 text-xs text-gray-400">2022. This site is still actively worked on and might look wonky at times.</span>
           </div>
          </div>
-          <div className="relative w-full h-full bg-neutral-200 text-neutral-800 md:w-1/2">
+          <div className="relative w-full h-full bg-neutral-800 text-neutral-200 md:w-1/2">
           <div className="container flex flex-col justify-end h-full p-4 pt-8 md:p-16">
             <h2 className="text-2xl font-semibold">Blog</h2>
             {posts.sort(
@@ -66,14 +60,14 @@ export default function Home({ posts }) {
                 ).map(({ slug, frontmatter }) => (
               <div
                 key={slug}
-                className='text-black'
+                className='text-neutral-200'
               >
                 <Link href={`/blog/${slug}`}>
                   <a>
                     <p className='pt-4 mb-0 text-lg underline'>{frontmatter.title}</p>
                   </a>
                 </Link>
-                <span className='text-sm text-neutral-700'>{frontmatter.publishedDate}</span>
+                <span className='text-sm text-neutral-200'>{frontmatter.publishedDate}</span>
               </div>
             ))}
           </div>
